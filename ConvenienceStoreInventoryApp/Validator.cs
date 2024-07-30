f33d30b9edcbeb8d7a640d71a7afa075cd9dd00b
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-07-30 03:05:21 -0700

    Added validator class, need to populate

---
+++ ConvenienceStoreInventoryApp/Validator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvenienceStoreInventoryApp
+{
+    static class Validator
+    {
+        /// <summary>
+        /// Checks if the Textbox Text property
+        /// has a non-empty, non-whitespace string
+        /// </summary>
+        /// <param name="box">The textbox to validate</param>
+        /// <returns>True if textbox contains non_whitespace data</returns>
+        public static bool IsPresent(TextBox box)
+        {
+            if (string.IsNullOrWhiteSpace(box.Text))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
