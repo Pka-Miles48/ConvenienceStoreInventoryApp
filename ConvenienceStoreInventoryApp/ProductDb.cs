eb066ec8b9c7077c16e68b123b0aed682764bb7a
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-07-30 03:05:01 -0700

    Created db class, want to see options for connecting to database without to much coding

---
+++ ConvenienceStoreInventoryApp/ProductDb.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvenienceStoreInventoryApp
+{
+    static class ProductDb
+    {
+        public static void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
