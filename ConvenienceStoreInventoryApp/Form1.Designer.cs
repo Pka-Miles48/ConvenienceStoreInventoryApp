dccc95177074e947b2e04a44a9de8a94b55bdf37
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-07-31 02:10:49 -0700

    Removed textboxes ont the form1 form since I did them on add product from already

--- ConvenienceStoreInventoryApp/Form1.Designer.cs
+++ ConvenienceStoreInventoryApp/Form1.Designer.cs
@@ -28,83 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            txtName = new TextBox();
-            txtCategory = new TextBox();
-            txtPrice = new TextBox();
-            txtStock = new TextBox();
             btnAddProduct = new Button();
             btnUpdateProduct = new Button();
             btnLookupProduct = new Button();
             btnProcessSale = new Button();
+            lstProducts = new ListBox();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(56, 59);
-            label1.Name = "label1";
-            label1.Size = new Size(42, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Name:";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(56, 123);
-            label2.Name = "label2";
-            label2.Size = new Size(58, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Category:";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(56, 187);
-            label3.Name = "label3";
-            label3.Size = new Size(36, 15);
-            label3.TabIndex = 2;
-            label3.Text = "Price:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(56, 251);
-            label4.Name = "label4";
-            label4.Size = new Size(39, 15);
-            label4.TabIndex = 3;
-            label4.Text = "Stock:";
-            // 
-            // txtName
-            // 
-            txtName.Location = new Point(150, 56);
-            txtName.Name = "txtName";
-            txtName.Size = new Size(100, 23);
-            txtName.TabIndex = 4;
-            // 
-            // txtCategory
-            // 
-            txtCategory.Location = new Point(150, 120);
-            txtCategory.Name = "txtCategory";
-            txtCategory.Size = new Size(100, 23);
-            txtCategory.TabIndex = 5;
-            // 
-            // txtPrice
-            // 
-            txtPrice.Location = new Point(150, 184);
-            txtPrice.Name = "txtPrice";
-            txtPrice.Size = new Size(100, 23);
-            txtPrice.TabIndex = 6;
-            // 
-            // txtStock
-            // 
-            txtStock.Location = new Point(150, 248);
-            txtStock.Name = "txtStock";
-            txtStock.Size = new Size(100, 23);
-            txtStock.TabIndex = 7;
             // 
             // btnAddProduct
             // 
@@ -146,42 +75,36 @@
             btnProcessSale.UseVisualStyleBackColor = true;
             btnProcessSale.Click += btnProcessSale_Click;
             // 
+            // lstProducts
+            // 
+            lstProducts.FormattingEnabled = true;
+            lstProducts.ItemHeight = 15;
+            lstProducts.Location = new Point(16, 30);
+            lstProducts.Name = "lstProducts";
+            lstProducts.Size = new Size(256, 184);
+            lstProducts.TabIndex = 12;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(486, 323);
+            Controls.Add(lstProducts);
             Controls.Add(btnProcessSale);
             Controls.Add(btnLookupProduct);
             Controls.Add(btnUpdateProduct);
             Controls.Add(btnAddProduct);
-            Controls.Add(txtStock);
-            Controls.Add(txtPrice);
-            Controls.Add(txtCategory);
-            Controls.Add(txtName);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private TextBox txtName;
-        private TextBox txtCategory;
-        private TextBox txtPrice;
-        private TextBox txtStock;
         private Button btnAddProduct;
         private Button btnUpdateProduct;
         private Button btnLookupProduct;
         private Button btnProcessSale;
+        private ListBox lstProducts;
     }
 }
