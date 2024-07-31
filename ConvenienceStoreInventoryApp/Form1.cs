146629ba3dfae795e221508a18e2bd03e24402e0
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-07-31 02:09:49 -0700

    removed classes in the form1 form and added form1 load even to populate a list on the form

--- ConvenienceStoreInventoryApp/Form1.cs
+++ ConvenienceStoreInventoryApp/Form1.cs
@@ -1,36 +1,10 @@
 namespace ConvenienceStoreInventoryApp
-{ 
+{
     public partial class Form1 : Form
     {
-        private Store store = new Store();
-
         public Form1()
         {
             InitializeComponent();
-        }
-
-        public class Store
-        {
-            // Define the properties and methods for the Store class here
-            // For example:
-            public List<Product> Products { get; set; }
-
-            public Store()
-            {
-                Products = new List<Product>();
-            }
-
-            // Add methods for adding products, updating products, etc.
-        }
-
-        public class Product
-        {
-            public string Name { get; set; }
-            public string Category { get; set; }
-            public decimal Price { get; set; }
-            public int Stock { get; set; }
-
-            // You can add more properties if needed
         }
 
         private void btnAddProduct_Click(object sender, EventArgs e)
@@ -54,5 +28,10 @@ namespace ConvenienceStoreInventoryApp
         {
             // Process sale logic
         }
-    } 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Product> products = new ConvenienceStoreInventoryApp.ProductDb.GetAllProducts();
+        }
+    }
 }
