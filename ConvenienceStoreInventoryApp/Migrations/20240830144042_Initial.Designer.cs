94d98120c62afd6bfd954fc0f05b87609a5f9344
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-08-30 07:49:53 -0700

    Updated database because product was not showing up in the table preventing me to add anything

--- ConvenienceStoreInventoryApp/Migrations/20240830144042_Initial.Designer.cs
+++ ConvenienceStoreInventoryApp/Migrations/20240830144042_Initial.Designer.cs
@@ -3,6 +3,7 @@ using ConvenienceStoreInventoryApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ConvenienceStoreInventoryApp.Migrations
 {
     [DbContext(typeof(ProductsContext))]
-    partial class ProductsContextModelSnapshot : ModelSnapshot
+    [Migration("20240830144042_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
