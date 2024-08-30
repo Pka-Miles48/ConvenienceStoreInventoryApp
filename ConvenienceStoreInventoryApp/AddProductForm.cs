b13d050752834cfa61979a040d17cf4f67094988
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-08-30 06:44:29 -0700

    Simplified form to work on database function

--- ConvenienceStoreInventoryApp/AddProductForm.cs
+++ ConvenienceStoreInventoryApp/AddProductForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConvenienceStoreInventoryApp.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,7 +16,6 @@ namespace ConvenienceStoreInventoryApp
         public AddProductForm()
         {
             InitializeComponent();
-            btnAddProduct.Enabled = false; //Disables add button until valid data is entered
         }
 
         private void AddProductForm_Load(object sender, EventArgs e)
@@ -26,40 +26,20 @@ namespace ConvenienceStoreInventoryApp
         private void btnAddProduct_Click(object sender, EventArgs e)
         {
             //Get info from the form
-            string productName = txtProductName.Text;
-            TextBox productPrice = txtPrice;
-            TextBox productQuantity = txtQuantity;
-            string category = txtCategory.Text;
-            string description = txtDescription.Text;
-
-            double price = Validator.ValidNumberInTextbox(productPrice);
-            double quantity = Validator.ValidNumberInTextbox(productQuantity);
-
-            if (Validator.ValidTextInput(productName, price, quantity, category, description) )
+            Product p = null;
+            try
             {
-                //Add info into a new product object
-                Product product = new Product(productName, price, quantity, category, description);
-
-                //Send object to ProductDb for storage
-                //ProductDb.Add(product);
-
-                //Show that the product was added to the db
-                MessageBox.Show("Added Product Successfully");
-                //close window
-                Close();
+                p = new Product(txtProductName.Text, double.Parse(txtPrice.Text), 
+                    double.Parse(txtQuantity.Text), txtCategory.Text, txtDescription.Text);
             }
-            else
+            catch (Exception ex)
             {
-                throw new ArgumentException("Entered invalid information.");
+                MessageBox.Show("Error creating product: " + ex.Message);
+                return;
             }
-            
-
-            
+            ProductDb.Add(p);
 
 
-
-
-            
         }
     }
 }
